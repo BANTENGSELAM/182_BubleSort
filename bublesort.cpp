11eb3df66aaa40c4d9bbae212df527f41fe85b67
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-03-06 15:42:32 +0700

    Input

--- bublesort.cpp
+++ bublesort.cpp
@@ -3,3 +3,32 @@ using namespace std;
 
 int a[20];//Deklarasi
 int n;
+
+void input () //Input
+{
+    while (true)
+    {
+        cout << "Masukan Panjang Array : ";
+        cin >> n;
+        
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Error : Maksimal input Array adalah 20\n";
+        }
+    }
+    
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "====================" << endl;
+    
+    for (int i = 0; i < n; i++)// i++ / i = i + i .....
+    {
+        cout << "Masukkan Nilai ke - " << i + 1 << " : ";
+        cin >> a[i];
+    }
+}
