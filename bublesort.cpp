fe0d839b7e184dc9360672803604403b39913b1f
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-03-06 15:45:23 +0700

    Display

--- bublesort.cpp
+++ bublesort.cpp
@@ -32,3 +32,17 @@ void input () //Input
         cin >> a[i];
     }
 }
+
+
+void display ()//Display
+{
+    cout << endl;
+    cout << "==========================" << endl;
+    cout << "Elemen Array telah terurut" << endl;
+    cout << "==========================" << endl;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke -" << i << " : " << a[i] << endl;
+    }
+}
